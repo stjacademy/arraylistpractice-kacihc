b532da7b6d0e8de433ba4e0b48ce480eb1c99da0
kacihc <kaci.cochran@stjlabs.com>
2019-02-11 11:24:38 -0500

    add notes for ArrayList

---
+++ ArrayListPractice/src/Practice.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		ArrayList <Integer> myList = new ArrayList  <Integer>();
+		
+		myList.add(42);
+		myList.add(25);
+		myList.add(315);
+		
+		//myList.remove(1);
+		//myList.get(1);
+		//myList.set(1, 50);
+		//myList.add(1, 50);
+		
+		for(int i=0; i<myList.size(); i++) {
+			System.out.println(myList.get(i));
+		}
+		
+		for(Integer num : myList) {
+			System.out.println(num);
+			myList.remove(1);
+
+		}
+		
+	}
+
+}
