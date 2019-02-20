df5a2060a3570f962f2a13d5d65f857c25c42257
kacihc <kaci.cochran@stjlabs.com>
2019-02-20 15:10:54 -0500

    Add factors

--- ArrayListPractice/src/ArrayListLab.java
+++ ArrayListPractice/src/ArrayListLab.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class ArrayListLab {
+public class ArrayListLab
+{
+	public static Scanner scan = new Scanner(System.in);
+	public static void main (String[]args) {
+		ArrayList a = getListOfFactors(36);
+		System.out.println(a);
+		
+	}
+	public static ArrayList<Integer> getListOfFactors(int number)
+	{
+		ArrayList<Integer>factors = new ArrayList<Integer>();
+		
+		for(int i=2; i<number; i++) {
+			if(number%i==0) {
+				factors.add(i);
+			}
+		}
+		
+		
+		
+		return factors;                                                                        
+	}
+/*
+	public static void keepOnlyCompositeNumbers( ArrayList<Integer> nums )
+	{
 
+		
+		
+	}
+	*/
 }
+
