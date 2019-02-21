4a0f455ceeb12fb7560e90ebef3d7c8f8989df6b
kacihc <kaci.cochran@stjlabs.com>
2019-02-21 12:14:29 -0500

    add composite

--- ArrayListPractice/src/ArrayListLab.java
+++ ArrayListPractice/src/ArrayListLab.java
@@ -1,35 +1,48 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ArrayListLab
-{
+public class ArrayListLab {
+
 	public static Scanner scan = new Scanner(System.in);
+
 	public static void main (String[]args) {
+
 		ArrayList a = getListOfFactors(36);
 		System.out.println(a);
-		
+
+
+		composite(a);
+
 	}
-	public static ArrayList<Integer> getListOfFactors(int number)
-	{
+
+	public static ArrayList<Integer> getListOfFactors(int number){
+
 		ArrayList<Integer>factors = new ArrayList<Integer>();
-		
+
 		for(int i=2; i<number; i++) {
 			if(number%i==0) {
 				factors.add(i);
 			}
 		}
-		
-		
-		
+
 		return factors;                                                                        
 	}
-/*
-	public static void keepOnlyCompositeNumbers( ArrayList<Integer> nums )
-	{
 
-		
-		
+	public static void composite( ArrayList<Integer> factors ) {
+
+		for(int i=0; i<factors.size(); i++) {
+
+			for(int j=0; j<Math.sqrt(i); j++) {
+
+				if(factors.get(j)%j == 0) {
+					factors.remove(factors.get(i)); 
+					i--;
+				}
+
+			}
+		}
+		System.out.println(factors);
 	}
-	*/
+
 }
 
